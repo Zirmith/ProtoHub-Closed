e1d30afbe85c16bc25e6fbe255e1da87e5dd990b
_Headsetz <f2v74yt@gmail.com>
2022-12-15 14:58:19 -0600

    Update current.lua

--- version/main/current.lua
+++ version/main/current.lua
@@ -1,5 +1,5 @@
 return {
-    ['Version'] = "0.0.3",
+    ['Version'] = "0.0.2",
     ['Type'] = "Core",
     ['Patched'] = "0.0.2",
     ['Description'] = "This is a test update please dis-reguard"
