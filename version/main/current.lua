0e6ba21973cffed3309a3debef6528b6098d438f
_Headsetz <f2v74yt@gmail.com>
2022-12-15 15:00:48 -0600

    Core Update v 0.0.3
    Lol

--- version/main/current.lua
+++ version/main/current.lua
@@ -1,6 +1,6 @@
 return {
-    ['Version'] = "0.0.2",
-    ['Type'] = "Core",
+    ['Version'] = "0.0.3",
+    ['Type'] = "Core Update",
     ['Patched'] = "0.0.2",
-    ['Description'] = "This is a test update please dis-reguard"
+    ['Description'] = "Added, Trigger Volcanio, Toggled / Button [ Removed Invis Due To Bug ], and return of Annoy Server | Currently Working On GunMods, And Robbery"
 }
