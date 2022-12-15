2fe185e79775b78f5f2ae399672bc1cd5643f777
_Headsetz <f2v74yt@gmail.com>
2022-12-14 18:38:50 -0600

    Update info.lua

--- updates/version/core/info.lua
+++ updates/version/core/info.lua
@@ -1,6 +1,8 @@
-return {
+local  info =  {
     ['Version'] = "0.0.3",
     ['Type'] = "Core"
     ['Patched'] = "0.0.2",
     ['Description'] = "This is a test update please dis-reguard"
 }
+
+return info
